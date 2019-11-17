7808b732bbf591185b698bf67dada4adc3de4b03
2017118121lin <2017118121@stu.hstc.edu.cn>
2019-11-17 16:57:25 +0800

    第5次作业 瀑布布局

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/ChangeActivity.java
@@ -0,0 +1,16 @@
+package com.example.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+
+
+    }
+}

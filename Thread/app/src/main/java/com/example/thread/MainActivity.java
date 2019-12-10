1612ab561cc8ff36055b69a4dd70a2580d958088
2017118121lin <2017118121@stu.hstc.edu.cn>
2019-12-10 19:29:46 +0800

    第6次作业

---
+++ Thread/app/src/main/java/com/example/thread/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.thread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        new Thread(new ThreadShow()).start();
+    }
+    // handler类接收数据
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                if(i==11){
+                    i=0;
+                }
+                text.setText(Integer.toString(i++));
+                System.out.println("receive....");
+            }
+        };
+    };
+
+    // 线程类
+    class ThreadShow implements Runnable {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler.sendMessage(msg);
+                    System.out.println("send...");
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    System.out.println("thread error...");
+                }
+            }
+        }
+    }
+}
+
+
+
+
